60371c3a4f49736e8571b98899d9ce930589fe03
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-13 13:25:02 +0600

    add header

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 - react-router-dom
 - react-icons
 - set routes for some pages
+
+### header part
+
+- add header component
+- add three section in navbar
+- add logo in header
+- add search bar in header
+- add signup and login Link in header (responsive for mobile view)
--- src/components/Pages/Header.jsx
+++ src/components/Pages/Header.jsx
@@ -1,14 +1,43 @@
+import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
 export default function Header() {
   return (
-    <header>
-      <ul>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-        <li>four</li>
-        <li>five</li>
-        <li>six</li>
-      </ul>
+    <header className="bg-slate-300 shadow-lg">
+      <div className="flex justify-between items-center  p-4 max-w-6xl mx-auto">
+        <h1 className="text-xs sm:text-xl font-bold flex flex-wrap">
+          <Link to="/">
+            Book <span className="text-orange-700">Swap</span>
+          </Link>
+        </h1>
+        <form className="bg-slate-100 rounded-lg flex items-center p-3">
+          <input
+            className="  bg-transparent rounded-lg focus:outline-none focus:shadow-none w-24 sm:w-64 "
+            type="text"
+            placeholder="Search"
+          />
+          <CiSearch />
+        </form>
+        <div className="flex items-center space-x-4">
+          <Link
+            to="/about"
+            className="hidden sm:inline-block text-sm sm:text-xl font-bold hover:underline"
+          >
+            About
+          </Link>
+          <Link
+            to="/login"
+            className="text-sm sm:text-xl font-bold hover:underline"
+          >
+            Login
+          </Link>
+          <Link
+            to="/signup"
+            className="hidden sm:inline-block text-sm sm:text-xl font-bold hover:underline"
+          >
+            Register
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
