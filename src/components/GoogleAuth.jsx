eeba9b20bab02546242d2eca9c01dc87d430d488
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-14 19:42:28 +0600

    implement google sign in

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -1,8 +1,52 @@
 import React from "react";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { app } from "../firebase.config";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "./redux/user";
 
 export default function GoogleAuth() {
+  const dispatch = useDispatch();
+  const handleGoogleSignIn = () => {
+    //  Sign in with Google
+    const auth = getAuth(app);
+    const googleProvider = new GoogleAuthProvider();
+    signInWithPopup(auth, googleProvider)
+      .then((res) => {
+        // console.log(res);
+        // send it backend
+        // need userName,email,and photoURL
+
+        // const {user}={res.user.displayName,res.user.email,res.user.photoURL}
+        const username = res.user.displayName;
+        const email = res.user.email;
+        const photoURL = res.user.photoURL;
+        const user = {
+          username,
+          email,
+          photoURL,
+        };
+        // send it backend
+        fetch("http://localhost:5000/signup/google", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.success === true) {
+              dispatch(signInSuccess(data));
+            }
+          })
+          .catch((e) => console.log(e));
+      })
+      .catch((e) => console.log(e));
+  };
   return (
     <button
+      onClick={handleGoogleSignIn}
       className="p-4 bg-green-950 text-white uppercase font-semibold rounded-lg"
       type="button"
     >
---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvT8nCGtmwpCmsNRPqd-4k1mnla67cN0A",
+  authDomain: "book-swap-64d94.firebaseapp.com",
+  projectId: "book-swap-64d94",
+  storageBucket: "book-swap-64d94.appspot.com",
+  messagingSenderId: "806573386924",
+  appId: "1:806573386924:web:a9de2931e18c16c0234309",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
