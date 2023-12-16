8bd08f0c797dcbbe3695888ff7a3d208a179e2ad
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-17 01:48:26 +0600

    implement page details,if others book chance to buy it

--- src/components/Pages/Books.jsx
+++ src/components/Pages/Books.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import Contact from "./Contact";
 
 export default function Books() {
   const { id } = useParams();
 
+  const { currentUser } = useSelector((state) => state.user);
   const [loading, setLoading] = useState(false);
   const [bookInfo, setBookInfo] = useState(null);
 
+  const [buyNow, setBuyNow] = useState(false);
   useEffect(() => {
     const getSingleBookInfo = () => {
       setLoading(false);
@@ -74,6 +78,19 @@ export default function Books() {
         ) : (
           ""
         )}
+      </div>
+      <div>
+        {currentUser &&
+          currentUser.userData._id !== bookInfo?.owner &&
+          !buyNow && (
+            <button
+              onClick={() => setBuyNow(true)}
+              className="w-1/2 ml-28 lg:w-full  lg:mx-0 md:mx-0 text-white bg-slate-800 p-4 rounded-lg my-6"
+            >
+              Buy Now
+            </button>
+          )}
+        {buyNow && <Contact bookInfo={bookInfo} />}
       </div>
     </div>
   );
---
+++ src/components/Pages/Contact.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Contact({ bookInfo }) {
+  //   console.log(bookInfo.owner);
+  const [user, setuser] = useState(null);
+  const [message, setMessage] = useState("");
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
+  console.log(`http://localhost:5000/user/${bookInfo?.owner}`);
+  useEffect(() => {
+    const loadUser = async () => {
+      fetch(`http://localhost:5000/user/${bookInfo?.owner}`, {
+        credentials: "include",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setuser(data.userData);
+        });
+    };
+    loadUser();
+  }, [bookInfo.owner]);
+
+  return (
+    <div className="my-8">
+      <h1 className="text-center font-semibold text-slate-900">
+        Contact with{" "}
+        <Link to="https://mail.google.com/mail/u/0/#inbox">
+          <span className="text-blue-400 mx-2">{user?.email}</span>
+        </Link>{" "}
+        for{" "}
+        <span className="text-2xl font-bold text-blue-800  ">
+          {bookInfo.title}{" "}
+        </span>
+        book{" "}
+      </h1>
+      <textarea
+        name=""
+        id="message"
+        cols="30"
+        placeholder="write here your message"
+        rows="4"
+        value={message}
+        onChange={handleMessage}
+        className="w-full border-2 border-gray-300 p-4 rounded-md bg-slate-200 focus:outline-none"
+      ></textarea>
+      <Link
+        to={`mailto:${user?.email}?subject=i need ${bookInfo?.title}&body=${message}`}
+      >
+        <button className="bg-slate-600 text-white p-4 rounded-lg">
+          Send message
+        </button>
+      </Link>
+    </div>
+  );
+}
